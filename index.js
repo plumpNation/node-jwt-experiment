6613f645c0af89a3ef1ba915f68b7844c6f198f3
Gavin King <gavin.king@itslearning.com>
2017-07-23 18:49:20 +0200

    went all async on yer ass

--- index.js
+++ index.js
@@ -14,4 +14,26 @@ const options = {
 
 const token = jwt.sign(payload, SECRET, options);
 
+console.info('token created');
 console.log(token);
+
+setTimeout(function () {
+    verifyJWT(payload, SECRET)
+        .then((jwt) => {
+            console.log(jwt);
+        })
+        .catch(err => console.error(err));
+}, 3000);
+
+function verifyJWT(payload, SECRET) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, SECRET, (err, jwt) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve(jwt);
+        })
+    });
+}
